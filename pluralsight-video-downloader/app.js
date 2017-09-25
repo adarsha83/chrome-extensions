51513d11e50974a0ef0177e916d15904449cf39d
Adarsha K S <ks.adarsha83@gmail.com>
2017-09-25 10:57:27 +0530

    Some

--- pluralsight-video-downloader/app.js
+++ pluralsight-video-downloader/app.js
@@ -1,12 +1,20 @@
-var fs = require('fs');
+var fs = require('browserify-fs');
 
-document.addEventListener('DOMContentLoaded', function() {
-    var link = document.getElementById('btn-download');
-    // onClick's logic below:
-    link.addEventListener('click', function() {
-        chrome.extension.getBackgroundPage().console.log("HERE2");
+fs.mkdir('/home', function() {
+    fs.writeFile('/home/hello-world.txt', 'Hello world!\n', function() {
+        fs.readFile('/home/hello-world.txt', 'utf-8', function(err, data) {
+            chrome.extension.getBackgroundPage().console.log(data);
+        });
     });
 });
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     var link = document.getElementById('btn-download');
+//     // onClick's logic below:
+//     link.addEventListener('click', function() {
+//         chrome.extension.getBackgroundPage().console.log("HERE2");
+//     });
+// });
 // $(function(){
 //     chrome.extension.getBackgroundPage().console.log("HERE1");
 //     var dropdown = document.getElementById("btn-download");
